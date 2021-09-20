fb50e460b56a1bceca57d1b26b38d777b500a519
Laziz Abdullaev <lazizabdullaev02@gmail.com>
2021-09-19 20:52:50 -0700

    refactor: utility generateMessage

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const { generateMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -20,24 +21,16 @@ io.on('connection', (socket) => {
     console.log('User diconnected');
   });
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to che chat app',
-    createdAt: new Date().getTime(),
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime(),
-  });
+  socket.broadcast.emit(
+    'newMessage',
+    generateMessage('Admin', 'New User joined')
+  );
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      ...message,
-      createdAt: new Date().getTime(),
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
     // socket.broadcast.emit('newMessage', {
     //   ...message,
     //   createdAt: new Date().getTime(),
