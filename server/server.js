f46f3792283123d526a5f958593e338c8cb1f511
Laziz Abdullaev <lazizabdullaev02@gmail.com>
2021-09-24 23:38:41 -0700

    Send messages to user's room only

--- server/server.js
+++ server/server.js
@@ -57,15 +57,24 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    const user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit(
+        'newMessage',
+        generateMessage(user.name, message.text)
+      );
+    }
+
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit(
+    const user = users.getUser(socket.id);
+
+    io.to(user.room).emit(
       'newLocationMessage',
-      generateLocationMessage('Admin', coords.latitude, coords.longitude)
+      generateLocationMessage(user.name, coords.latitude, coords.longitude)
     );
   });
 });
